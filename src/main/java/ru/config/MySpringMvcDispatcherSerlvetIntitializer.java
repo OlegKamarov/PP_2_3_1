bd091a6c3e0a7c16179b232627d30d0240b24e37
Oleg Kamarov <oleg471@ya.ru>
2022-08-29 08:07:40 +0300

    решена. проблемы с кодировкой

--- src/main/java/ru/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ src/main/java/ru/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -1,8 +1,10 @@
 package ru.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
@@ -26,6 +28,16 @@ public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotation
     public void onStartup(ServletContext aServletContext) throws ServletException {
         super.onStartup(aServletContext);
         registerHiddenFieldFilter(aServletContext);
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[] {characterEncodingFilter};
+
+        //пытался исправить проблемы с кодировкой, решение не помогло
     }
 
     private void registerHiddenFieldFilter(ServletContext aContext) {
